71ce63984e9472ac442b0c5cf1013d6e27ec0df9
Pradheepa P <pradheepa_p@discovery.com>
2020-10-20 22:31:20 +0800

    update handler

--- handler.js
+++ handler.js
@@ -6,5 +6,19 @@ module.exports.getEventsDS = async event => {
       name : `Jane's Birthday Party`,
       date: '13-Oct-2020'
      },
+     { 
+      name : `AWS Meetup`,
+      date: '21-Oct-2020'
+     },
+     {
+      date: '23-Oct-2020'
+     },
   ]
 };
+
+
+module.exports.getLocationDS = async event => {
+  
+  return "Zoom";
+    
+ };
